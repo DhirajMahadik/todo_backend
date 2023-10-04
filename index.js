2a9408ac1c5ce1f69a56fae4e98d25d7a3e03de8
Dhiraj Mahadik <dhirajmahadik9221@gmail.com>
2023-10-04 18:20:18 +0530

    completed-tasks endpoind

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.post('/api/register', (req, res) => {
             connect.query('insert into users(email,password) values(?,?)', [email, hash], (error, response) => {
                 if (error) res.status(400).send({ error: error.message.slice(0, error.message.length - 23) })
                 res.send(response)
-                console.log(response)
             })
         })
 
@@ -31,17 +30,14 @@ app.post('/api/register', (req, res) => {
 // Endpoint for login
 
 app.post('/api/login', (req, res) => {
-    console.log(req.body)
     const email = req.body.email;
     let query = 'Select password,id from users where email = ?'
     connect.query(query, [email], async (error, response) => {
         if (error) res.status(500).send({ error: 'some error occurs' });
         if (response.length !== 0) {
             let pass = await bcrypt.compare(req.body.password, response[0].password);
-            console.log(pass)
             if (pass) {
                 let id = response[0].id
-                console.log(id)
                 JWT.sign({ id }, 'dhiraj', { expiresIn: '900s' }, (error, token) => {
                     if (error) res.status(500).send({ error: 'some error occurs' });
                     res.send({ token })
@@ -60,9 +56,7 @@ app.post('/api/login', (req, res) => {
 // Verifying token
 
 const verifyToken = (req, res, next) => {
-    console.log(req.headers)
     let token = req.headers['authorization'];
-    console.log(token)
     if (typeof token === 'undefined') {
         res.status(401).send({ error: 'unauthorized user' })
     } else {
@@ -87,55 +81,90 @@ app.post('/api/tasks', verifyToken, (req, res) => {
 
 })
 
-app.post('/api/add-task', verifyToken, (req,res)=>{
+app.post('/api/completed-tasks', verifyToken, (req, res) => {
+
     JWT.verify(req.token, 'dhiraj', (error, authData) => {
-        console.log(error)
         if (error) res.status(400).send({ error: 'session timeout' });
-        connect.query('insert into tasks (task , user_id) values (?,?)', [req.body.task ,authData.id], (error, response) => {
+        connect.query('select task, task_id  from completed_tasks where user_id = ? order by task_id desc', [authData.id], (error, response) => {
             if (error) res.status(400).send({ error: error.message.slice(0, error.message.length - 23) })
-            res.send({message:'Task added successfully'})
+            res.send(response)
+        })
+    })
+
+
+})
+
+app.post('/api/clear-completed-tasks', verifyToken, (req, res) => {
+
+    JWT.verify(req.token, 'dhiraj', (error, authData) => {
+        if (error) res.status(400).send({ error: 'session timeout' });
+        connect.query(' delete  from completed_tasks where user_id = ?', [authData.id], (error, response) => {
+            if (error) res.status(400).send({ error: 'Something went wrong' })
+            res.send({message:'History cleared successfully'})
+        })
+    })
+
+
+})
+
+
+app.post('/api/add-task', verifyToken, (req, res) => {
+    JWT.verify(req.token, 'dhiraj', (error, authData) => {
+        if (error) res.status(400).send({ error: 'session timeout' });
+        connect.query('insert into tasks (task , user_id) values (?,?)', [req.body.task, authData.id], (error, response) => {
+            if (error) res.status(400).send({ error: error.message.slice(0, error.message.length - 23) })
+            res.send({ message: 'Task added successfully' })
         })
     })
 })
 
-app.post('/api/update-task', verifyToken, (req,res)=>{
+app.post('/api/update-task', verifyToken, (req, res) => {
     JWT.verify(req.token, 'dhiraj', (error, authData) => {
-        console.log(error)
         if (error) res.status(400).send({ error: 'session timeout' });
-        connect.query('update  tasks set task=? where task_id = ?', [req.body.task , req.body.task_id], (error, response) => {
+        connect.query('update  tasks set task=? where task_id = ?', [req.body.task, req.body.task_id], (error, response) => {
             if (error) res.status(400).send({ error: error.message.slice(0, error.message.length - 23) })
-            res.send({message:'Task updated successfully'})
+            res.send({ message: 'Task updated successfully' })
         })
     })
 })
 
-// connect.connect()
-app.listen(5500, (error) => {
-    if (error) throw error;
-    console.log('server is running')
+app.post('/api/complete-task', verifyToken, (req, res) => {
+    JWT.verify(req.token, 'dhiraj', (error, authData) => {
+        if (error) res.status(400).send({ error: 'session timeout' });
+        connect.query('delete from tasks where task_id = ?', [req.body.task_id], (error, response) => {
+            if (error) res.status(400).send({ error: error.message.slice(0, error.message.length - 23) })
+            if (response.affectedRows === 1) {
+                connect.query('insert into completed_tasks (task,user_id) values (?,?)', [req.body.task, authData.id], (error, response) => {
+                    if (error) res.status(400).send({ error: error.message })
+                    res.send({ message: 'Mark as completed successfully' })
+                })
+            } else {
+                res.status(400).send({ error: 'something went wrong' })
+            }
+
+        })
+    })
 })
 
-// connect.query('Select*from tasks;', (err,res)=>{
-//     if(err) throw err;
-//     console.log(res)
-// })
 
-// connect.query('insert into users (email) values(?)',['dhirajMahadik@gmail.com'] ,(err,res)=>{
-//     if(err) throw err;
-//     console.log(res)
-// })
 
-// connect.query('insert into tasks (user_id, task) values(?,?)',['11','go to school everyday '] ,(err,res)=>{
-//     if(err) throw err;
-//     console.log(res)
-// })
+app.post('/api/delete-task', verifyToken, (req, res) => {
+    JWT.verify(req.token, 'dhiraj', (error, authData) => {
+        if (error) res.status(400).send({ error: 'session timeout' });
+        connect.query('delete from tasks where task_id = ?', [req.body.task_id], (error, response) => {
+            if (error) res.status(400).send({ error: error.message.slice(0, error.message.length - 23) })
+            res.send({ message: 'Task deleted successfully' })
+        })
+    })
+})
 
-// connect.query('select task from tasks where user_id = 2 ' ,(err,res)=>{
-//     if(err) throw err;
-//     console.log(res)
-//     res.map((element)=>{
-//         console.log(element.task)
-//     })
-// })
+connect.connect((error)=>{
+    if(error) throw error;
+    app.listen(5500, (error) => {
+        if (error) throw error;
+        console.log('server is running')
+    })
+})
+
 
 
